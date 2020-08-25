3133e285d8089137265724b1d8257ea676f90edc
Laurence Aitchison <laurence.aitchison@gmail.com>
2020-08-25 10:47:10 +0100

    happy?

---
+++ main.py
@@ -0,0 +1,221 @@
+'''Train CIFAR10 with PyTorch.'''
+from __future__ import print_function
+import sys
+sys.path.append('..')
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.distributions as td
+
+from torch.distributions import Categorical
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+
+from experiments.models import *
+import numpy as np
+import pandas as pd
+import random
+
+parser = argparse.ArgumentParser(description='cSG-MCMC CIFAR10 Training')
+parser.add_argument('output_filename', type=str, nargs='?', default='test')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--alpha', type=float, default=0.9,
+                    help='1: SGLD; <1: SGHMC')
+parser.add_argument('--seed', type=int, default=1,
+                    help='random seed')
+parser.add_argument('--S', type=int, default=1, help='inverse temperature')
+parser.add_argument('--cycle', type=int, default=50,  help='cycle length')
+parser.add_argument('--M', type=int, default=4,  help='number of cycles')
+parser.add_argument('--noise_epochs', type=int, default=45,  help='epoch in cycle after which we add noise')
+parser.add_argument('--sample_epochs', type=int, default=47, help='epoch in cycle after which we save samples')
+parser.add_argument('--trainset', default='train', nargs='?', choices=["train", "test", "cifar10h"])
+
+args = parser.parse_args()
+use_cuda = torch.cuda.is_available()
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+temp = 1/args.S
+
+
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='data', train=(args.trainset=="train"), download=True, transform=transform_train)
+if args.trainset=="cifar10h":
+    cifar10h = np.load("cifar10h.npy").astype(np.float)
+    trainset.targets = cifar10h
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='data', train=(args.trainset!="train"), download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=0)
+
+
+
+# Model
+print('==> Building model..')
+net = ResNet18()
+if use_cuda:
+    net.cuda()
+    cudnn.benchmark = True
+    #cudnn.deterministic = True
+
+def update_params(lr,epoch):
+    for p in net.parameters():
+        if not hasattr(p,'buf'):
+            p.buf = torch.zeros(p.size()).cuda()
+        d_p = p.grad.data
+        d_p.add_(p.data, alpha=weight_decay)
+        buf_new = (1-args.alpha)*p.buf - lr*d_p
+        if (epoch%args.cycle)+1>args.noise_epochs:
+            eps = torch.randn(p.size()).cuda()
+            buf_new += (2.0*lr*args.alpha*temp/datasize)**.5*eps
+        p.data.add_(buf_new)
+        p.buf = buf_new
+
+def adjust_learning_rate(epoch, batch_idx):
+    rcounter = epoch*num_batch+batch_idx
+    cos_inner = np.pi * (rcounter % (T // args.M))
+    cos_inner /= T // args.M
+    cos_out = np.cos(cos_inner) + 1
+    lr = 0.5*cos_out*lr_0
+    if args.trainset == "cifar10h":
+        lr = lr / 50.
+    return lr
+
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    train_likelihood = td.Multinomial if args.trainset=="cifar10h" else td.Categorical
+
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        net.zero_grad()
+        lr = adjust_learning_rate(epoch,batch_idx)
+        outputs = net(inputs)
+        #loss = criterion(outputs, targets)
+        loss = -train_likelihood(logits=outputs).log_prob(targets).mean()
+        loss.backward()
+        update_params(lr,epoch)
+
+        train_loss += loss.data.item()
+        predicted = torch.argmax(outputs, -1)
+        if args.trainset == "cifar10h":
+            targets = torch.argmax(targets, -1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).cpu().sum()
+        if batch_idx%100==0:
+            print('Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+def test(epoch):
+    with torch.no_grad():
+        net.eval()
+        test_loss = 0
+        correct = 0
+        total = 0
+        outputs = []
+
+        for inputs, targets in testloader:
+            inputs, targets = inputs.cuda(), targets.cuda()
+            output = net(inputs)
+            #loss = F.cross_entropy(output, targets)
+            loss = -td.Categorical(logits=output).log_prob(targets).mean()
+
+            test_loss += loss.data.item()
+            predicted = torch.argmax(output.data, -1)
+            total += targets.size(0)
+            correct += predicted.eq(targets.data).cpu().sum()
+
+            outputs.append(output.detach().cpu())
+
+        print(outputs[0].shape)
+
+
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss/len(testloader), correct, total,
+        100. * correct / total))
+
+    return torch.cat(outputs, 0)
+
+weight_decay = 5e-4
+datasize = len(trainset)
+num_batch = datasize/args.batch_size+1
+lr_0 = 0.5 # initial lr
+T = args.M*args.cycle*num_batch # total number of iterations
+criterion = nn.CrossEntropyLoss()
+mt = 0
+
+#### Save 10000 x 10 output matrix for all sampled networks
+outputs = []
+for epoch in range(args.M*args.cycle):
+    train(epoch)
+    if (epoch%args.cycle)+1>args.sample_epochs: # save 3 models per cycle
+        output = test(epoch)
+        outputs.append(output)
+
+
+######## Evaluate accuracy using saved matrices
+targetss = []
+for _, targets in testloader:
+    targetss.append(targets)
+targets = torch.cat(targetss, 0)
+
+#[Samples, 10000, 10]
+outputs = torch.stack(outputs, 0)
+
+#### Compute preds
+#[10000, 10]: compute probs and average over networks
+Py = F.softmax(outputs, -1).mean(0)
+pred = torch.argmax(Py, -1)
+correct = (pred == targets).sum().item()
+total = pred.shape[0]
+
+#### Compute test-log-likelihood.
+Py = Categorical(logits=outputs)
+#[Samples, 10000]: compute log-prob for targets
+log_Py = Py.log_prob(targets)
+#[10000]: average over networks
+log_Py = torch.logsumexp(log_Py, 0) - math.log(log_Py.shape[0])
+loss = log_Py.mean().item()
+
+print(correct)
+print(total)
+print(loss)
+
+pd.DataFrame({
+  'loss': [loss],
+  'error' : [1-correct/total]
+}).to_csv(args.output_filename)
+#for epoch in range(args.epochs):
+#    train(epoch)
+#    test(epoch)
+#    if (epoch%50)+1>47: # save 3 models per cycle
+#        print('save!')
+#        net.cpu()
+#        torch.save(net.state_dict(),args.dir + '/cifar_csghmc_%i.pt'%(mt))
+#        mt += 1
+#        net.cuda(device_id)
---
+++ makefile
@@ -0,0 +1,26 @@
+bp_gpu = lsub -g 1 -m 48 --autoname --cmd
+
+#General functions
+a1 = $(word 1,$(subst _, , $(1)))
+a2 = $(word 2,$(subst _, , $(1)))
+a3 = $(word 3,$(subst _, , $(1)))
+a4 = $(word 4,$(subst _, , $(1)))
+a5 = $(word 5,$(subst _, , $(1)))
+a6 = $(word 6,$(subst _, , $(1)))
+
+results/test_%: main.py
+	$(bp_gpu) python $< $@ --trainset test --S $* --cycle 150 --noise_epochs 120 --sample_epochs 130 --M 8
+results/cifar10h_%: main.py
+	$(bp_gpu) python $< $@ --trainset cifar10h --S $* --cycle 150 --noise_epochs 120 --sample_epochs 130 --M 8
+results/train_%: main.py
+	$(bp_gpu) python $< $@ --trainset train --S $*
+
+trainset_list = test_ cifar10h_
+S_list = 10 3 1
+
+path_trainset = $(addprefix results/,$(trainset_list))
+path_trainset_S = $(foreach pre,$(path_trainset),$(addprefix $(pre),$(S_list))) 
+cifar10_test: $(path_trainset_S)
+
+cifar10_train: $(addprefix results/train_,$(S_list))
+
